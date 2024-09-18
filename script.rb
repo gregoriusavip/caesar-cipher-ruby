7d706e0847a2dff21cbc66c9798372b386704e11
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-09-17 21:53:59 -0700

    Format using rubocop

--- script.rb
+++ script.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 def alphabet?(char)
   (char.ord >= 65 && char.ord <= 90) || (char.ord >= 97 && char.ord <= 122)
 end
 
 def shift_alphabet(char, num_shift)
-  char.ord >= 97 ? range = (97..122) : range = (65..90)
+  range = char.ord >= 97 ? (97..122) : (65..90)
   char = char.ord + num_shift
   case char
   when range
@@ -16,7 +18,7 @@ def shift_alphabet(char, num_shift)
 end
 
 def caesar_cipher(string, num_shift)
-  result = ""
-  string.each_char { |char| alphabet?(char) ? result += shift_alphabet(char, num_shift) : result += char }
+  result = ''
+  string.each_char { |char| result += alphabet?(char) ? shift_alphabet(char, num_shift) : char }
   result
 end
